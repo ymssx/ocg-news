6427cb642e21fbd7572ca86a0659b5a785103dab
YAMI <yangming.yami@bytedance.com>
2023-12-12 19:58:46 +0800

    feat: SEO optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+export const metadata = {
+  title: 'YuGiOh News',
+  description: 'This is an informational website that provides news about Yu-Gi-Oh!',
+}
+
 export default function Home() {
   return (
-    <main className="p-8">
-      <h1>YuGoOh OCG News</h1>
-      <ul>
-        <li>
+    <main className="p-8 text-center">
+      <h1>{metadata.title}</h1>
+      <p>{metadata.description}</p>
+      <ul className="flex gap-2 justify-center mt-8">
+        <li className="list-none">
           <a href="/package/list">Package List</a>
         </li>
       </ul>

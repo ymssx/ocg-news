a11707277a178c2d4cc57f3fa0599b7d473feca3
YAMI <yangming.yami@bytedance.com>
2023-12-13 12:19:30 +0800

    feat: icon & Home description

--- app/package/list/page.tsx
+++ app/package/list/page.tsx
@@ -7,7 +7,7 @@ const PackageList = async () => {
   const listInto: PackageData[] = await Promise.all(list.map(async (id) => (await import(`@/data/package/${id}.json`)).default));
   return (
     <div className="p-8">
-      <h1>卡包</h1>
+      <h1>Packages</h1>
       <ul className="">
         {listInto.map(({ id, name, desc }) => (
           <li key={id}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import Image from "next/image";
+
 export const metadata = {
   title: 'YuGiOh News',
-  description: 'This is an informational website that provides news about Yu-Gi-Oh!',
+  description: 'Big welcome to Labrynth!',
 }
 
 export default function Home() {
   return (
     <main className="p-8 text-center">
-      <h1>{metadata.title}</h1>
+      <h1>白銀の城のラビュリンス</h1>
       <p>{metadata.description}</p>
+      <p>ビッグウェルカム・ラビュリンス</p>
+      <div className="flex justify-center mt-8">
+        <Image src="/images/labrynth.jpeg" width={300} height={300} alt="Labrynth" />
+      </div>
       <ul className="flex gap-2 justify-center mt-8">
         <li className="list-none">
           <a href="/package/list">Package List</a>

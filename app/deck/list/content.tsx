4167e2e1b247e2599709ae1d67cbe95e038feaf2
yami <yamiiyang@tencent.com>
2024-06-06 00:20:20 +0800

    fix: 修复编译报错

--- app/deck/list/content.tsx
+++ app/deck/list/content.tsx
@@ -1,14 +1,23 @@
 'use client';
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function DeckList({ decks }: { decks: string[] }) {
+  const [lang, setlang] = useState('ja-JP');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setlang(window.navigator.language);
+    }
+  }, []);
+
   return (
     <div>
       <h1 className="font-bold">上位卡组</h1>
       <ol>
         {decks.map(item => (
           <li key={item}>
-            <Link href={`/${navigator.language}/deck/detail/${item}`}>{item}</Link>
+            <Link href={`/${lang}/deck/detail/${item}`}>{item}</Link>
           </li>
         ))}
       </ol>
